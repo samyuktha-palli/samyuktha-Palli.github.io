07a2be08d174b661526dc65b4e37a43b2a9724ce
Raja <n.rtheja@outlook.com>
2020-09-19 20:54:58 -0400

    intial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    let text = document.querySelector('#sasha-tran');
+    let circle = document.querySelector('.cls-1');
+    text.classList.add('animate-logo-text');
+    circle.classList.add('animate-cls-1');
+});
